3c13ddad3015d8a236dbc8f56fe14542cfdd97a0
Jakub Dobrý <jakub.dobry8@gmail.com>
2024-10-06 22:58:47 +0200

    Load PDFViewer on the client again

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -3,9 +3,13 @@ import {Button} from "../../components/ui/button";
 import {pdf, PDFViewer} from '@react-pdf/renderer';
 import {saveAs} from "file-saver";
 import {ResumeDocument} from "../pdf/resume";
+import dynamic from "next/dynamic";
 import {useSearchParams} from "next/navigation";
 import {Suspense} from "react";
 
+const PDFViewerClient = dynamic(() => Promise.resolve(PDFViewer), {
+  ssr: false,
+});
 
 function Resume() {
   const searchParams = useSearchParams()
@@ -22,9 +26,9 @@ function Resume() {
   return (
     <div className="relative">
       <Button onClick={generatePdfDocument} className="absolute left-0 top-0 z-40">Download</Button>
-      <PDFViewer width="100%" height="100%" className="absolute h-dvh">
+      <PDFViewerClient width="100%" height="100%" className="absolute h-dvh">
         <ResumeDocument email={email} phone={phone}/>
-      </PDFViewer>
+      </PDFViewerClient>
     </div>
   )
 }
